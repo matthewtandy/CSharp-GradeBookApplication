0776808274b76dd57467ec54dfa6a6276f744b4e
matthewtandy <matthew.tandy@slguk.com>
2018-05-02 10:26:24 +0100

    type not types
    type not types

--- GradeBook/Enums/GradeBookType.cs
+++ GradeBook/Enums/GradeBookType.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GradeBook.Enums
+﻿namespace GradeBook.Enums
 {
     
     
-        public enum GradebookTypes
+        public enum GradebookType
         {
             standard,
             Ranked,
